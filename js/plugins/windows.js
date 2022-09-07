3df0cccbc11af0d59d66d87add7afcacabaf43fb
era3effulgence <112944891+era3effulgence@users.noreply.github.com>
2022-09-07 22:54:58 +0800

    Update windows.js

--- js/plugins/windows.js
+++ js/plugins/windows.js
@@ -4,3 +4,22 @@ SceneManager._boxWidth          = 1632;
 SceneManager._boxHeight         = 720;
 //一格48*48
 //目前為34*15
+
+
+
+
+
+ var this._switchStretchMode();
+            break;
+
+
+/**
+ * @static
+ * @method _switchStretchMode
+ * @return {Boolean}
+ * @private
+ */
+Graphics._switchStretchMode = function() {
+    this._stretchEnabled = !this._stretchEnabled;
+    this._updateAllElements();
+};
