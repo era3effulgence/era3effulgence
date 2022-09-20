c9e0eef220516fb52d2638988354cc35901a87f7
era3effulgence <112944891+era3effulgence@users.noreply.github.com>
2022-09-21 00:02:08 +0800

    a

---
+++ js/plugins/windows.js
@@ -0,0 +1,6 @@
+SceneManager._screenWidth       = 1632;
+SceneManager._screenHeight      = 720;
+SceneManager._boxWidth          = 1632;
+SceneManager._boxHeight         = 720;
+//一格48*48
+//目前為34*15
